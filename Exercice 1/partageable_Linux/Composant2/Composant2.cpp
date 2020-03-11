28fd57163615d266e9382e7e6f92520db6456c70
simonikane <simona.arizanova@gmail.com>
2020-03-11 14:35:51 +0000

    t

--- Exercice 1/partageable_Linux/Composant2/Composant2.cpp
+++
@@ -1,6 +0,0 @@
-#include "Composant2.h"
-
-int composant2(int p1, int p2)
-{
-	return p1*p2;
-}
--- Exercice 1/partageable_Linux/interfaces/Composant2.h
+++
@@ -1,7 +0,0 @@
-#ifndef COMPOSANT2_H
-#define COMPOSANT2_H
-
-// interface du composant2
-
-int composant2(int p1, int p2);
-#endif
